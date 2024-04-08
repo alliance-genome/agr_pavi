157d602bedff4dec487a1647d054e57e90022fa4
Manuel Luypaert <luypaert@ebi.ac.uk>
2024-04-08 18:01:05 +0100

    Added CDK resource assertion tests

---
+++ pipeline/aws_infra/tests/test_cdk_infra_stack.py
@@ -0,0 +1,36 @@
+"""
+Unit testing for the cdk_infra_stack,
+to ensure breaking changes are caught and handled
+before getting applied to the live AWS resources.
+"""
+
+import aws_cdk
+import aws_cdk.assertions as assertions
+
+from cdk_classes.cdk_infra_stack import CdkInfraStack
+
+app = aws_cdk.App()
+stack = CdkInfraStack(app, "pytest-stack")
+template = assertions.Template.from_stack(stack)
+
+# If any of the below ECR repository names change, then ensure this change is intentional.
+# If so, take below manual steps before merging to ensure correct PAVI deployment and execution:
+#  * Update all references to respective repository name in all PAVI code to match the new name
+#  * Update RepositoryName in below unit test(s) to match the new name
+#  * The old ECR repository will not be cleanup up automatically, so after merging and deployment:
+#    * Move All images from the old ECR repository to the new one
+#      (or delete the images if no longer relevant)
+#    * Delete the old ECR repository
+def test_pipeline_seq_retrieval_ecr_repo():
+    template.has_resource(type=aws_cdk.aws_config.ResourceType.ECR_REPOSITORY.compliance_resource_type, props={
+        "Properties": {
+            "RepositoryName": "agr_pavi/seq_retrieval"
+        }
+    })
+
+def test_pipeline_alignment_ecr_repo():
+    template.has_resource(type=aws_cdk.aws_config.ResourceType.ECR_REPOSITORY.compliance_resource_type, props={
+        "Properties": {
+            "RepositoryName": "agr_pavi/alignment"
+        }
+    })
