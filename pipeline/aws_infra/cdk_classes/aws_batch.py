4e1b431676218f20c4d95f57e37c760fe9e16eb7
Manuel Luypaert <luypaert@ebi.ac.uk>
2024-04-11 15:57:19 +0100

    Enabled import of shared ECR repos and S3 workdir bucket in (cloned) CDK environments

--- pipeline/aws_infra/cdk_classes/aws_batch.py
+++ pipeline/aws_infra/cdk_classes/aws_batch.py
@@ -7,14 +7,24 @@ from aws_cdk import (
     aws_s3 as s3
 )
 
+from typing import Optional
+
 
 class PaviExecutionEnvironment:
 
     compute_environment: aws_batch.FargateComputeEnvironment
     job_queue: aws_batch.JobQueue
-    nf_workdir_bucket: s3.Bucket
+    nf_workdir_bucket: s3.Bucket | s3.IBucket
 
-    def __init__(self, scope: Stack, env_suffix: str) -> None:
+    def __init__(self, scope: Stack, env_suffix: str, shared_work_dir_bucket: Optional[str]) -> None:
+        """
+        Defines the PAVI execution environment.
+
+        Args:
+            scope: CDK Stack to which the construct belongs
+            env_suffix: environment suffix, added to created resource names
+            shared_work_dir_bucket: when defined, use S3 bucket with defined the value as bucketName as Nextflow workdir bucket
+        """
 
         vpc = ec2.Vpc.from_lookup(scope, "AgrVpc", vpc_id="vpc-55522232")
         subnets = ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS)
@@ -24,7 +34,7 @@ class PaviExecutionEnvironment:
         if env_suffix:
             ce_name += f'_{env_suffix}'
         self.compute_environment = aws_batch.FargateComputeEnvironment(
-            scope=scope, id=f'pavi-pipeline-compute-environment',
+            scope=scope, id='pavi-pipeline-compute-environment',
             compute_environment_name=ce_name,
             vpc=vpc, vpc_subnets=subnets,
             spot=True,
@@ -44,17 +54,22 @@ class PaviExecutionEnvironment:
 
         self.job_queue.add_compute_environment(self.compute_environment, order=1)  # type: ignore
 
-        # Create an S3 bucket to contain the nextflow work dir
-        bucket_name = 'agr-pavi-pipeline-nextflow'
-        if env_suffix:
-            bucket_name += f'-{env_suffix}'
-        self.nf_workdir_bucket = s3.Bucket(
-            scope=scope, id='pavi-pipeline-nf-workdir-bucket',
-            bucket_name=bucket_name,
-            access_control=s3.BucketAccessControl.PRIVATE,
-            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
-            auto_delete_objects=False,
-            public_read_access=False,
-            removal_policy=RemovalPolicy.RETAIN,
-            versioned=False
-        )
+        if not shared_work_dir_bucket:
+            # Create an S3 bucket to contain the nextflow work dir
+            bucket_name = 'agr-pavi-pipeline-nextflow'
+            if env_suffix:
+                bucket_name += f'-{env_suffix}'
+            self.nf_workdir_bucket = s3.Bucket(
+                scope=scope, id='pavi-pipeline-nf-workdir-bucket',
+                bucket_name=bucket_name,
+                access_control=s3.BucketAccessControl.PRIVATE,
+                block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+                auto_delete_objects=False,
+                public_read_access=False,
+                removal_policy=RemovalPolicy.RETAIN,
+                versioned=False
+            )
+        else:
+            self.nf_workdir_bucket = s3.Bucket.from_bucket_name(
+                scope=scope, id='pavi-pipeline-nf-workdir-bucket',
+                bucket_name=shared_work_dir_bucket)
--- pipeline/aws_infra/cdk_classes/pavi_ecr_repo.py
+++ pipeline/aws_infra/cdk_classes/pavi_ecr_repo.py
@@ -5,21 +5,26 @@ from aws_cdk import (
 )
 
 
-class PaviEcrRepository:
-
-    repository: ecr.Repository
+class PaviEcrRepository(ecr.Repository):
 
     def __init__(self, scope: Stack, id: str, component_name: str, env_suffix: str) -> None:
+        """
+        Initialise a ecr.Repository instance with common setting and naming that apply to all PAVI repositories
 
+        Args:
+            scope: CDK Stack to which the construct belongs
+            id: ID used to uniquely identify the construct withing the given scope
+            component_name: PAVI component name, which will be used to name the repository.
+            env_suffix: Deployment environment suffix, added to created repository name
+
+        Yields:
+            aws_ecr.Repository
+        """
         # Create the ECR repository
         PAVI_REPO_PREFIX = 'agr_pavi/'
         repository_name = PAVI_REPO_PREFIX + component_name
         if env_suffix:
             repository_name += f'_{env_suffix}'
 
-        repo = ecr.Repository(scope, id=id, repository_name=repository_name,
-                              empty_on_delete=False, removal_policy=RemovalPolicy.RETAIN)
-        self.repository = repo
-
-    def get_repo_arn(self) -> str:
-        return str(self.repository.repository_arn)
+        super().__init__(scope, id=id, repository_name=repository_name,
+                         empty_on_delete=False, removal_policy=RemovalPolicy.RETAIN)
