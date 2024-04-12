7efd3a842a9c927b2a10dda5dd921120bd78353a
Manuel Luypaert <luypaert@ebi.ac.uk>
2024-04-12 12:06:41 +0100

    Added dev AWS stack definition for temporary stack deployment and testing

--- pipeline/aws_infra/cdk_app.py
+++ pipeline/aws_infra/cdk_app.py
@@ -9,4 +9,10 @@ app = App()
 CdkInfraStack(app, "PaviPipelineCdkStack",
               env=agr_aws_environment)
 
+CdkInfraStack(app, "PaviPipelineCdkStack-dev", env_suffix="dev",
+              shared_seq_retrieval_image_repo='agr_pavi/pipeline_seq_retrieval',
+              shared_alignment_image_repo='agr_pavi/pipeline_alignment',
+              shared_work_dir_bucket='agr-pavi-pipeline-nextflow',
+              env=agr_aws_environment)
+
 app.synth()
