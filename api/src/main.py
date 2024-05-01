fad3b01cd52a83235797db1db79d25f074ea2949
Manuel Luypaert <luypaert@ebi.ac.uk>
2024-05-01 20:45:16 +0100

    Added basic API job status tracking and reporting

--- api/src/main.py
+++ api/src/main.py
@@ -1,4 +1,4 @@
-from fastapi import BackgroundTasks, FastAPI
+from fastapi import BackgroundTasks, FastAPI, HTTPException
 from pydantic import BaseModel
 
 import json
@@ -12,8 +12,9 @@ class Pipeline_seq_region(BaseModel):
     seq_regions: list[str|dict[str,str|int]]
     fasta_file_url: str
 
-class Pipeline_response_post(BaseModel):
-    uuid: str
+class Pipeline_job(BaseModel):
+    uuid: UUID
+    status: str = 'pending'
 
 def run_pipeline(pipeline_seq_regions: list[Pipeline_seq_region], uuid: UUID) -> None:
     """
@@ -23,6 +24,8 @@ def run_pipeline(pipeline_seq_regions: list[Pipeline_seq_region], uuid: UUID) ->
         pipeline_seq_regions: sequence regions for pipeline input
         uuid: UUID to uniquely identify the job being run
     """
+    jobs[uuid].status = 'running'
+
     model_dumps: list = []
     for seq_region in pipeline_seq_regions:
         model_dumps.append(seq_region.model_dump())
@@ -36,15 +39,25 @@ def run_pipeline(pipeline_seq_regions: list[Pipeline_seq_region], uuid: UUID) ->
                     '--input_seq_regions_file', seqregions_filename,
                     '--publish_dir', f'pipeline-results_{uuid}'])
 
+    jobs[uuid].status = 'completed'
+
 app = FastAPI()
+jobs: dict[UUID, Pipeline_job] = {}
 
 @app.get("/")
 async def help_msg():
     return {"help": "Welcome to the PAVI API! For more information on how to use it, see the docs at {host}/docs"}
 
 @app.post('/pipeline-job/', status_code=201)
-async def post_pipeline_job(pipeline_seq_regions: list[Pipeline_seq_region], background_tasks: BackgroundTasks) -> Pipeline_response_post:
-    job_uuid: UUID = uuid1()
-    background_tasks.add_task(func=run_pipeline, pipeline_seq_regions=pipeline_seq_regions, uuid=job_uuid)
+async def create_new_pipeline_job(pipeline_seq_regions: list[Pipeline_seq_region], background_tasks: BackgroundTasks) -> Pipeline_job:
+    new_task: Pipeline_job = Pipeline_job(uuid=uuid1())
+    jobs[new_task.uuid] = new_task
+    background_tasks.add_task(func=run_pipeline, pipeline_seq_regions=pipeline_seq_regions, uuid=new_task.uuid)
 
-    return Pipeline_response_post(uuid=str(job_uuid))
+    return new_task
+
+@app.get("/pipeline-job/{uuid}")
+async def get_pipeline_job_details(uuid: UUID):
+    if uuid not in jobs.keys():
+        raise HTTPException(status_code=404, detail='Job not found.')
+    return jobs[uuid]
