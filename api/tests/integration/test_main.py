17637b9b2b1c14b2d2cfb919093eaf1daec9f150
Manuel Luypaert <luypaert@ebi.ac.uk>
2024-05-09 22:33:47 +0100

    Enabled running API integration testing on external API server through pytest.
    Usefull to test deployed code or externally run API server (such as container).

--- api/tests/integration/test_main.py
+++ api/tests/integration/test_main.py
@@ -1,14 +1,23 @@
 from fastapi.testclient import TestClient
 
 from main import app
-from os import environ, getcwd
-from time import sleep
+from os import environ, getcwd, getenv
 from uuid import UUID
 
-client = TestClient(app)
+from .helper_fns import poll_job_progress
 
-environ["API_RESULTS_PATH_PREFIX"] = f'{getcwd()}/'
-environ["API_EXECUTION_ENV"] = 'local'
+from httpx import Client
+
+
+external_api_base_url = getenv('EXTERNAL_API_BASE_URL')
+
+client: TestClient | Client
+if external_api_base_url:
+    client = Client(base_url=external_api_base_url)
+else:
+    client = TestClient(app)
+    environ["API_RESULTS_PATH_PREFIX"] = f'{getcwd()}/'
+    environ["API_EXECUTION_ENV"] = 'local'
 
 
 def test_pipeline_workflow():
@@ -26,25 +35,8 @@ def test_pipeline_workflow():
 
     job_uuid: UUID = response_dict['uuid']
 
-    # Poll pipeline progress untill completed (or timeout)
-    walltime = 0
-    TIMEOUT = 10 * 60  # Timeout after 10 Minutes
-    INTERVAL = 30  # Poll every 30 seconds
-    while walltime < TIMEOUT:
-        response = client.get(f'/pipeline-job/{job_uuid}')
-        assert response.status_code == 200
-
-        response_dict: dict = response.json()
-        assert all(key in response_dict.keys() for key in ['uuid', 'status'])
-        assert response_dict['uuid'] == job_uuid
-
-        if response_dict['status'] == 'completed':
-            break
-        else:
-            sleep(INTERVAL)
-            walltime += INTERVAL
-
-    assert walltime < TIMEOUT, 'API failed to report job completion before timeout.'
+    # Poll pipeline progress until completed (or timeout)
+    poll_job_progress(client, job_uuid)
 
     # Collect and compare pipeline result
     response = client.get(f'/pipeline-job/{job_uuid}/alignment-result')
