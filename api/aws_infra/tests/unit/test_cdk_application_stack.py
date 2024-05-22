2140ab8434b60ff426019e86a6fae1650954a269
Manuel Luypaert <luypaert@ebi.ac.uk>
2024-05-22 12:13:26 +0100

    Added EB environment testing to unit tests

--- api/aws_infra/tests/unit/test_cdk_application_stack.py
+++ api/aws_infra/tests/unit/test_cdk_application_stack.py
@@ -8,7 +8,7 @@ from aws_cdk import App
 from aws_cdk.aws_config import ResourceType
 import aws_cdk.assertions as assertions
 
-from cdk_classes.cdk_application_stack import CdkEBApplicationStack
+from cdk_classes.cdk_application_stack import CdkEBApplicationStack, CdkApplicationStack
 
 from pathlib import Path
 from sys import path as sys_path
@@ -21,15 +21,33 @@ sys_path.append(str(repo_root_path))
 from shared_aws_infra.agr_aws_env import agr_aws_environment  # noqa: E402
 
 app = App()
-stack = CdkEBApplicationStack(app, "pytest-EBApplication-stack", env=agr_aws_environment)
-template = assertions.Template.from_stack(stack)
+eb_app_stack = CdkEBApplicationStack(app, "pytest-api-EB-Application-stack", env=agr_aws_environment)
+
+eb_env_stack = CdkApplicationStack(
+    app, "pytest-api-env-stack", eb_app_stack, 'pytest', env=agr_aws_environment)
+
+eb_app_template = assertions.Template.from_stack(eb_app_stack)
+eb_env_template = assertions.Template.from_stack(eb_env_stack)
 
 
 # If below application name changes, then ensure this change is intentional.
 # Such change may possibility break deploying the same application version to multiple environments
 def test_eb_application() -> None:
-    template.has_resource(type=ResourceType.ELASTIC_BEANSTALK_APPLICATION.compliance_resource_type, props={
+    eb_app_template.has_resource(type=ResourceType.ELASTIC_BEANSTALK_APPLICATION.compliance_resource_type, props={
         "Properties": {
             "ApplicationName": "PAVI-api"
+        }
+    })
+
+
+# If below environment name changes, then ensure this change is intentional.
+# The environment name change could potentially break DNS rules or have unexpected deployment consequences
+# (several environment getting mashed together if prefixing did not happen appropriately)
+# All EB environments must belong to 'PAVI-api' EB application.
+def test_eb_app_version() -> None:
+    eb_env_template.has_resource(type=ResourceType.ELASTIC_BEANSTALK_ENVIRONMENT.compliance_resource_type, props={
+        "Properties": {
+            "ApplicationName": "PAVI-api",
+            "EnvironmentName": "PAVI-api-pytest"
         }
     })
