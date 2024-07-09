9f99286aa8daf7f431c4121ff63f9f53f222a6c7
Manuel Luypaert <luypaert@ebi.ac.uk>
2024-07-09 15:55:50 +0100

    Added webUI CNAME unit testing

--- webui/aws_infra/tests/unit/test_application_cdk_stack.py
+++ webui/aws_infra/tests/unit/test_application_cdk_stack.py
@@ -21,7 +21,8 @@ eb_app_stack = WebUiEbApplicationCdkStack(
 eb_env_stack = WebUiEbEnvironmentCdkStack(
     app, "pytest-webui-env-stack", env_suffix='pytest',
     eb_app_stack=eb_app_stack,
-    env=agr_aws_environment)
+    env=agr_aws_environment,
+    prod_cname=True)
 
 eb_app_template = assertions.Template.from_stack(eb_app_stack)
 eb_env_template = assertions.Template.from_stack(eb_env_stack)
@@ -41,10 +42,32 @@ def test_eb_application() -> None:
 # The environment name change could potentially break DNS rules or have unexpected deployment consequences
 # (several environment getting mashed together if prefixing did not happen appropriately)
 # All EB environments must belong to 'PAVI-webui' EB application.
-def test_eb_app_version() -> None:
+def test_eb_app_environment() -> None:
     eb_env_template.has_resource(type=ResourceType.ELASTIC_BEANSTALK_ENVIRONMENT.compliance_resource_type, props={
         "Properties": {
             "ApplicationName": "PAVI-webui",
             "EnvironmentName": "PAVI-webui-pytest"
+        }
+    })
+
+
+def test_env_cname() -> None:
+    eb_env_template.has_resource(type='AWS::Route53::RecordSet', props={
+        "Properties": {
+            "Name": "pytest-pavi.alliancegenome.org.",
+            "ResourceRecords": [{
+                "Fn::GetAtt": ["ebenvironment", "EndpointURL"]
+            }]
+        }
+    })
+
+
+def test_prod_cname() -> None:
+    eb_env_template.has_resource(type='AWS::Route53::RecordSet', props={
+        "Properties": {
+            "Name": "pavi.alliancegenome.org.",
+            "ResourceRecords": [{
+                "Fn::GetAtt": ["ebenvironment", "EndpointURL"]
+            }]
         }
     })
