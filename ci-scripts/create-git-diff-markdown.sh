b770667c454387493d7a283c70b0cf5bc9eb2881
Manuel Luypaert <luypaert@ebi.ac.uk>
2024-07-10 19:44:58 +0100

    Added missing create-git-diff-markdown.sh script

---
+++ ci-scripts/create-git-diff-markdown.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+FILEPATH=$1
+OUTDIR=$2
+OUTFILE=${OUTDIR}/${FILEPATH}.md
+
+install -D /dev/null ${OUTFILE}
+echo '<details>' > ${OUTFILE}
+echo '<summary>'${FILEPATH}'</summary>' > ${OUTFILE}
+echo '' > ${OUTFILE}
+echo '```diff' > ${OUTFILE}
+git diff -- ${FILEPATH} > ${OUTDIR}/${FILEPATH}.md
+echo '```diff' > ${OUTFILE}
+echo '</details>' > ${OUTFILE}
+
+echo ${OUTFILE}
