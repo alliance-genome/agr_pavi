b5fd7bf7af79c141e00e18fb81eece349bc69778
Manuel Luypaert <luypaert@ebi.ac.uk>
2024-09-05 10:39:46 +0100

    Added JobSubmitForm unit testing

---
+++ webui/src/app/components/client/__tests__/JobSubmitForm.test.tsx
@@ -0,0 +1,35 @@
+import { describe, expect, it } from '@jest/globals';
+
+import { render } from '@testing-library/react'
+import { JobSubmitForm } from '../JobSubmitForm/JobSubmitForm'
+
+jest.mock('https://raw.githubusercontent.com/alliance-genome/agr_ui/main/src/lib/utils.js',
+  () => {
+      return {
+          getSpecies: jest.fn(() => {}),
+          getSingleGenomeLocation: jest.fn(() => {})
+      }
+  },
+  {virtual: true}
+)
+
+describe('AlignmentEntry', () => {
+  it('renders a data-entry form', () => {
+    const result = render(
+      <JobSubmitForm agrjBrowseDataRelease='7.3.0' />
+    )
+
+    const inputGroup = result.container.querySelector('div.p-inputgroup')
+    expect(inputGroup).not.toBeNull()  // Expect (at least one) input group to be found
+  })
+
+  it('renders a submit button that is disabled by default', () => {
+    const result = render(
+      <JobSubmitForm agrjBrowseDataRelease='7.3.0' />
+    )
+ 
+    const submitBtn = result.container.querySelector('button[aria-label="Submit"]')
+    expect(submitBtn).not.toBeNull()  // Expect submit button to be found
+    expect(submitBtn).toBeDisabled()
+  })
+})
