ab04e1eaa57c25756f8d0fb278189c5dbecb70ef
Manuel Luypaert <luypaert@ebi.ac.uk>
2024-10-18 16:02:42 +0100

    Added endpoint for pipeline job log retrieval

--- api/src/main.py
+++ api/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, BackgroundTasks, FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
+from io import StringIO
 from os import getenv
 from pydantic import BaseModel
 from smart_open import open  # type: ignore
@@ -26,6 +27,10 @@ class Pipeline_seq_region(BaseModel):
 class Pipeline_job(BaseModel):
     uuid: UUID
     status: str = 'pending'
+    name: str
+
+    def __init__(self, uuid: UUID):
+        super().__init__(uuid=uuid, name=f'pavi-job-{uuid}')
 
 
 class HTTP_exception_response(BaseModel):
@@ -40,7 +45,9 @@ def run_pipeline(pipeline_seq_regions: list[Pipeline_seq_region], uuid: UUID) ->
         pipeline_seq_regions: sequence regions for pipeline input
         uuid: UUID to uniquely identify the job being run
     """
-    jobs[uuid].status = 'running'
+    job: Pipeline_job = jobs[uuid]
+
+    job.status = 'running'
 
     model_dumps: list[dict[str, Any]] = []
     for seq_region in pipeline_seq_regions:
@@ -53,16 +60,19 @@ def run_pipeline(pipeline_seq_regions: list[Pipeline_seq_region], uuid: UUID) ->
 
     try:
         subprocess.run(
-            ['./nextflow.sh', 'run', '-profile', api_execution_env, 'protein-msa.nf',
+            ['./nextflow.sh', 'run',
+             '-profile', api_execution_env,
+             '-name', job.name,
+             'protein-msa.nf',
              '--image_tag', api_pipeline_image_tag,
              '--input_seq_regions_file', seqregions_filename,
              '--publish_dir_prefix', api_results_path_prefix,
              '--publish_dir', f'pipeline-results_{uuid}'],
             check=True)
     except subprocess.CalledProcessError:
-        jobs[uuid].status = 'failed'
+        job.status = 'failed'
     else:
-        jobs[uuid].status = 'completed'
+        job.status = 'completed'
 
 
 app = FastAPI()
@@ -71,6 +81,14 @@ router = APIRouter(
 )
 
 jobs: dict[UUID, Pipeline_job] = {}
+
+
+def get_pipeline_job(uuid: UUID) -> Pipeline_job | None:
+    if uuid not in jobs.keys():
+        # TODO: log warning
+        return None
+    else:
+        return jobs[uuid]
 
 
 @router.get("/")
@@ -93,10 +111,12 @@ async def create_new_pipeline_job(pipeline_seq_regions: list[Pipeline_seq_region
 
 
 @router.get("/pipeline-job/{uuid}", response_model_exclude_none=True, responses={404: {'model': HTTP_exception_response}})
-async def get_pipeline_job_details(uuid: UUID) -> Pipeline_job:
-    if uuid not in jobs.keys():
+async def get_pipeline_job_handler(uuid: UUID) -> Pipeline_job:
+    job: Pipeline_job | None = get_pipeline_job(uuid)
+    if job is None:
         raise HTTPException(status_code=404, detail='Job not found.')
-    return jobs[uuid]
+    else:
+        return job
 
 
 @router.get("/pipeline-job/{uuid}/alignment-result", responses={404: {'model': HTTP_exception_response}})
@@ -113,5 +133,29 @@ async def get_pipeline_job_alignment_result(uuid: UUID) -> StreamingResponse:
                 yield from file_like
 
         return StreamingResponse(iterfile(), media_type="text/plain")
+
+
+@router.get("/pipeline-job/{uuid}/logs", responses={404: {'model': HTTP_exception_response}})
+async def get_pipeline_job_logs(uuid: UUID) -> StreamingResponse:
+    job: Pipeline_job | None = get_pipeline_job(uuid)
+    if job is None:
+        raise HTTPException(status_code=404, detail='Job not found.')
+
+    try:
+        result = subprocess.run(
+            ['./nextflow.sh', 'log', job.name, '-f', 'stderr,stdout'],
+            check=True, capture_output=True, text=True)
+    except subprocess.CalledProcessError:
+        # TODO: log error
+        raise HTTPException(status_code=500, detail='Error occured while retrieving logs.')
+    else:
+        def contentStream():  # type: ignore
+            with StringIO(result.stdout) as file_like:
+                yield from file_like
+
+            with StringIO(result.stderr) as file_like:
+                yield from file_like
+
+        return StreamingResponse(contentStream(), media_type="text/plain")
 
 app.include_router(router)
