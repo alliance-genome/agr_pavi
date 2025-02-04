b0582035f578f43bbaceb7234259635c2f316c9e
Manuel Luypaert <luypaert@ebi.ac.uk>
2025-02-04 19:17:02 +0000

    Cleanup

--- webui/src/app/result/components/__tests__/InteractiveAlignment.test.tsx
+++ webui/src/app/result/components/__tests__/InteractiveAlignment.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 // Mock lit elements, because jest otherwise trips over @lit/react
