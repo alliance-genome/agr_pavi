358cde89d3d852fdf5a82faa43b49cc27a25a57f
Manuel Luypaert <luypaert@ebi.ac.uk>
2025-02-13 10:40:45 +0000

    Ensure missing visual-regression baselines dont pass unnoticed

--- webui/cypress-image-diff.config.cjs
+++ webui/cypress-image-diff.config.cjs
@@ -2,7 +2,8 @@ const config = {
     ROOT_DIR: 'cypress/visual-tests/',
     JSON_REPORT: {
         OVERWRITE: true
-    }
+    },
+    FAIL_ON_MISSING_BASELINE: true
 };
 
 module.exports = config
