b03b68218d928edcd0c7df1f5bd745095b9db6b9
Manuel Luypaert <luypaert@ebi.ac.uk>
2025-03-17 17:09:13 +0000

    Enabled retrieving alleles from all categories that contain variants

--- webui/src/app/submit/components/AlignmentEntry/serverActions.ts
+++ webui/src/app/submit/components/AlignmentEntry/serverActions.ts
@@ -48,11 +48,58 @@ export async function fetchGeneInfo (geneId: string): Promise<GeneInfo|undefined
 export async function fetchAlleles (geneId: string): Promise<AlleleInfo[]> {
     console.log(`New gene info request received.`)
 
+    const endpointUrl = `https://www.alliancegenome.org/api/gene/${geneId}/allele-variant-detail`
+
+    // Retrieve all alleleCategory filter values
+    const alleleCategories = fetch(`${endpointUrl}?filter.alleleCategory=findCategories`, {
+        method: 'GET',
+        headers: {
+            'accept': 'application/json'
+        },
+
+    })
+    .then((response: Response) => {
+        if ( 500 <= response.status && response.status <= 599 ){
+            // No point in attempting to process the body, as no body is expected.
+            throw new Error('Server error received.', {cause: 'server error'})
+        }
+
+        return Promise.all([Promise.resolve(response), response.json()]);
+    })
+    .then(([response, body]) => {
+        if (response.ok) {
+            console.log(`Allele category query filters received successfully: ${JSON.stringify(body['supplementalData'])}`)
+            return Array.from(body['supplementalData']['distinctFieldValues']['filter.alleleCategory']);
+        } else {
+            const errMsg = 'Failure response received from gene/allele-variant-detail API.'
+            console.error(errMsg)
+            if( 400 <= response.status && response.status <= 499 ){
+                throw new Error(errMsg, {cause: 'user error'})
+            }
+            else{
+                console.log('Non user-error response received:', response)
+                throw new Error(errMsg, {cause: 'unkown'})
+            }
+
+        }
+    })
+
+    // Remove the "allele" option from alleleCategories
+    const searchAlleleCategories = (await alleleCategories).filter(category => category !== 'allele')
+
+    // Throw error if searchAlleleCategories is empty
+    if(  searchAlleleCategories.length === 0 ){
+        throw new Error('No search allele categories found.', {cause: 'unkown'})
+    }
+
     const queryParams = new URLSearchParams()
-    queryParams.append('filter.alleleCategory', 'variant')
+    // queryParams.append('filter.alleleCategory', searchAlleleCategories.map((value) => (encodeURIComponent(String(value)))).join('|'))
+    queryParams.append('filter.alleleCategory', searchAlleleCategories.join('|'))
+
+    console.log(`Fetching allele info for gene ${geneId} with query params: ${queryParams.toString()}`)
 
     //TODO: fetch all pages
-    const jobResponse = fetch(`https://www.alliancegenome.org/api/gene/${geneId}/allele-variant-detail?${queryParams.toString()}`, {
+    const jobResponse = fetch(`${endpointUrl}?${queryParams.toString()}`, {
         method: 'GET',
         headers: {
             'accept': 'application/json'
