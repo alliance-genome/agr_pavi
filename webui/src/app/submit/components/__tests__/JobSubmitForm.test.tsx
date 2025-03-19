fe90da2ff6b9113539d78483499b49e360e36f21
Manuel Luypaert <luypaert@ebi.ac.uk>
2025-03-19 09:57:59 +0000

    Added JobSubmitForm field-presence testing

--- webui/src/app/submit/components/__tests__/JobSubmitForm.test.tsx
+++ webui/src/app/submit/components/__tests__/JobSubmitForm.test.tsx
@@ -30,6 +30,13 @@ describe('AlignmentEntry', () => {
 
         const inputGroup = result.container.querySelector('div.p-inputgroup')
         expect(inputGroup).not.toBeNull()  // Expect (at least one) input group to be found
+
+        // Expect gene input to be found
+        expect(inputGroup?.querySelector('input[id="gene"]')).not.toBeNull()
+        // Expect alleles multiselect to be found
+        expect(inputGroup?.querySelector('div[id="alleles"].p-multiselect')).not.toBeNull()
+        // Expect transcripts multiselect to be found
+        expect(inputGroup?.querySelector('div[id="transcripts"].p-multiselect')).not.toBeNull()
     })
 
     it('renders a submit button that is disabled by default', () => {
