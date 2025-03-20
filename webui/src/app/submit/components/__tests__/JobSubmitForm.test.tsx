0bcad23cb5d59deb30b01bc5b37084d08a3152af
Manuel Luypaert <luypaert@ebi.ac.uk>
2025-03-20 18:16:50 +0000

    Added unit testing for JobSubmitForm add and remove record buttons

--- webui/src/app/submit/components/__tests__/JobSubmitForm.test.tsx
+++ webui/src/app/submit/components/__tests__/JobSubmitForm.test.tsx
@@ -1,6 +1,6 @@
 import { describe, expect, it } from '@jest/globals';
 
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import { JobSubmitForm } from '../JobSubmitForm/JobSubmitForm'
 
 jest.mock('https://raw.githubusercontent.com/alliance-genome/agr_ui/main/src/lib/utils.js',
@@ -42,5 +42,45 @@ describe('AlignmentEntry', () => {
         const submitBtn = result.container.querySelector('button[aria-label="Submit"]')
         expect(submitBtn).not.toBeNull()  // Expect submit button to be found
         expect(submitBtn).toBeDisabled()
+    })
+
+    it('add record button is functional', () => {
+        const result = render(
+            <JobSubmitForm agrjBrowseDataRelease='8.0.0' />
+        )
+
+        // Expect exactly one input group to be found
+        const inputGroup = result.container.querySelectorAll('div.p-inputgroup')
+        expect(inputGroup).toHaveLength(1)
+
+        // Expect exactly one add-record button to be found
+        const addRecordBtn = result.container.querySelector('button#add-record')
+        expect(addRecordBtn).not.toBeNull()
+
+        // Clicking the add-record button should add a new input group (exactly one extra, so two in total)
+        fireEvent.click(addRecordBtn!)
+
+        const inputGroups = result.container.querySelectorAll('div.p-inputgroup')
+        expect(inputGroups).toHaveLength(2)
+    })
+
+    it('remove record button is functional', () => {
+        const result = render(
+            <JobSubmitForm agrjBrowseDataRelease='8.0.0' />
+        )
+
+        // Expect exactly one input group to be found
+        const initialInputGroups = result.container.querySelectorAll('div.p-inputgroup')
+        expect(initialInputGroups).toHaveLength(1)
+
+        // Expect exactly one remove-record button to be found
+        const removeRecordBtn = result.container.querySelectorAll('button#remove-record')
+        expect(removeRecordBtn).toHaveLength(1)
+
+        // Clicking the add-record button should remove a input group (exactly one extra, so 0 left)
+        fireEvent.click(removeRecordBtn[0])
+
+        const newInputGroups = result.container.querySelectorAll('div.p-inputgroup')
+        expect(newInputGroups).toHaveLength(0)
     })
 })
