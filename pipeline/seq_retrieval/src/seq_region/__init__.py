fac79e2e8d2102318ae8c523972b6e48dab8514e
Manuel Luypaert <luypaert@ebi.ac.uk>
2025-04-08 11:34:35 +0100

    Added variant overlap methods and functions testing

--- pipeline/seq_retrieval/src/seq_region/__init__.py
+++ pipeline/seq_retrieval/src/seq_region/__init__.py
@@ -6,3 +6,4 @@ from .seq_region import SeqRegion
 from .multipart_seq_region import MultiPartSeqRegion
 from .translated_seq_region import TranslatedSeqRegion
 from .variant import Variant
+from .variant import variants_overlap
--- pipeline/seq_retrieval/tests/unit/seq_region/test_variant.py
+++ pipeline/seq_retrieval/tests/unit/seq_region/test_variant.py
@@ -2,7 +2,7 @@
 Unit testing for Variant class and related functions
 """
 
-from src.seq_region import Variant
+from src.seq_region import Variant, variants_overlap
 
 import pytest
 
@@ -58,3 +58,25 @@ def test_variant_from_id_initiation(wb_variant_yn10: Variant) -> None:
     variant = Variant.from_variant_id(VARIANT_ID)
     assert isinstance(variant, Variant)
     assert variant == wb_variant_yn10
+
+
+def test_variant_overlaps(wb_variant_yn32: Variant, wb_variant_yn30: Variant, wb_variant_yn10: Variant) -> None:
+    """
+    Test Variant.overlaps() method.
+    """
+    assert wb_variant_yn32.overlaps(wb_variant_yn30) is False
+    assert wb_variant_yn30.overlaps(wb_variant_yn32) is False
+    assert wb_variant_yn32.overlaps(wb_variant_yn10) is True
+    assert wb_variant_yn10.overlaps(wb_variant_yn32) is True
+    assert wb_variant_yn30.overlaps(wb_variant_yn10) is False
+    assert wb_variant_yn10.overlaps(wb_variant_yn30) is False
+
+
+def test_variants_overlap(wb_variant_yn10: Variant, wb_variant_yn30: Variant, wb_variant_yn32: Variant) -> None:
+    """
+    Test variants_overlap() function.
+    """
+    assert variants_overlap(list((wb_variant_yn10, wb_variant_yn30, wb_variant_yn32))) is True
+    assert variants_overlap(list((wb_variant_yn10, wb_variant_yn32))) is True
+    assert variants_overlap(list((wb_variant_yn10, wb_variant_yn30))) is False
+    assert variants_overlap(list((wb_variant_yn30, wb_variant_yn32))) is False
