23e3cb0be085ac0aee0b7ed2b7231cd50e3e43a0
Manuel Luypaert <luypaert@ebi.ac.uk>
2025-04-09 11:42:36 +0100

    Refactored translated_seq_region fixtures into separated fixtures files

--- pipeline/seq_retrieval/tests/unit/seq_region/conftest.py
+++ pipeline/seq_retrieval/tests/unit/seq_region/conftest.py
@@ -1,3 +1,4 @@
 
 from .fixtures.variants import *  # noqa: F401, F403
 from .fixtures.multipart_seq_regions import *  # noqa: F401, F403
+from .fixtures.translated_seq_region import *  # noqa: F401, F403
