659dae9e8db8a64ba325bce35b88df0133a71bca
Manuel Luypaert <luypaert@ebi.ac.uk>
2025-04-09 18:22:45 +0100

    Added MultipartSeqRegion sub_region method unit-testing

--- pipeline/seq_retrieval/tests/unit/seq_region/fixtures/multipart_seq_regions.py
+++ pipeline/seq_retrieval/tests/unit/seq_region/fixtures/multipart_seq_regions.py
@@ -33,3 +33,43 @@ def wb_cdna_c54h2_5_1() -> MultiPartSeqRegion:
     seq_region_list = [exon_1, exon_2, exon_3, exon_4]
 
     return MultiPartSeqRegion(seq_region_list)
+
+
+@pytest.fixture
+def wb_cdna_c42d8_1_1() -> MultiPartSeqRegion:
+    # WB:WBGene00016599 Transcript:C42D8.1.1
+    # Exon 1 (mRNA start)
+    exon_1: SeqRegion = SeqRegion(seq_id='X', start=5109506, end=5109644, strand='+',
+                                  fasta_file_url=FASTA_FILE_URL)
+
+    # Exon 2
+    exon_2: SeqRegion = SeqRegion(seq_id='X', start=5110473, end=5110556, strand='+',
+                                  fasta_file_url=FASTA_FILE_URL)
+
+    # Exon 3
+    exon_3: SeqRegion = SeqRegion(seq_id='X', start=5110610, end=5110708, strand='+',
+                                  fasta_file_url=FASTA_FILE_URL)
+
+    # Exon 4
+    exon_4: SeqRegion = SeqRegion(seq_id='X', start=5110762, end=5110869, strand='+',
+                                  fasta_file_url=FASTA_FILE_URL)
+
+    # Exon 5
+    exon_5: SeqRegion = SeqRegion(seq_id='X', start=5111054, end=5111200, strand='+',
+                                  fasta_file_url=FASTA_FILE_URL)
+
+    # Exon 6
+    exon_6: SeqRegion = SeqRegion(seq_id='X', start=5111250, end=5111309, strand='+',
+                                  fasta_file_url=FASTA_FILE_URL)
+
+    # Exon 7
+    exon_7: SeqRegion = SeqRegion(seq_id='X', start=5111423, end=5111473, strand='+',
+                                  fasta_file_url=FASTA_FILE_URL)
+
+    # Exon 8
+    exon_8: SeqRegion = SeqRegion(seq_id='X', start=5112256, end=5112426, strand='+',
+                                  fasta_file_url=FASTA_FILE_URL)
+
+    seq_region_list = [exon_1, exon_2, exon_3, exon_4, exon_5, exon_6, exon_7, exon_8]
+
+    return MultiPartSeqRegion(seq_region_list)
--- pipeline/seq_retrieval/tests/unit/seq_region/test_multipart_seq_region.py
+++ pipeline/seq_retrieval/tests/unit/seq_region/test_multipart_seq_region.py
@@ -43,3 +43,41 @@ def test_rel_position(wb_cdna_c54h2_5_1: MultiPartSeqRegion) -> None:
         wb_cdna_c54h2_5_1.to_rel_position(5780723)
     with pytest.raises(ValueError):
         wb_cdna_c54h2_5_1.to_rel_position(5778874)
+
+
+def test_sub_region_neg_strand(wb_cdna_c54h2_5_1: MultiPartSeqRegion) -> None:
+
+    # Test subregion extraction within a single exon
+    sub_region = wb_cdna_c54h2_5_1.sub_region(rel_start=101, rel_end=200)
+    assert isinstance(sub_region, MultiPartSeqRegion)
+    assert sub_region.seq_length == 100
+    assert sub_region.get_sequence() == wb_cdna_c54h2_5_1.get_sequence()[100:200]
+    assert sub_region.end == 5780564
+    assert sub_region.start == sub_region.end - (100 - 1)
+
+    # Test subregion extraction of multiple seqRegions
+    sub_region = wb_cdna_c54h2_5_1.sub_region(rel_start=1, rel_end=110)
+    assert isinstance(sub_region, MultiPartSeqRegion)
+    assert sub_region.seq_length == 110
+    assert sub_region.get_sequence() == wb_cdna_c54h2_5_1.get_sequence()[0:110]
+    assert sub_region.start == 5780555
+    assert sub_region.end == wb_cdna_c54h2_5_1.end
+
+
+def test_sub_region_pos_strand(wb_cdna_c42d8_1_1: MultiPartSeqRegion) -> None:
+
+    # Test subregion extraction within a single seqRegions (exon)
+    sub_region = wb_cdna_c42d8_1_1.sub_region(rel_start=2, rel_end=101)
+    assert isinstance(sub_region, MultiPartSeqRegion)
+    assert sub_region.seq_length == 100
+    assert sub_region.get_sequence() == wb_cdna_c42d8_1_1.get_sequence()[1:101]
+    assert sub_region.start == wb_cdna_c42d8_1_1.start + 2 - 1
+    assert sub_region.end == sub_region.start + 100 - 1
+
+    # Test subregion extraction accross multiple seqRegions (exons)
+    sub_region = wb_cdna_c42d8_1_1.sub_region(rel_start=150, rel_end=250)
+    assert isinstance(sub_region, MultiPartSeqRegion)
+    assert sub_region.seq_length == 101
+    assert sub_region.get_sequence() == wb_cdna_c42d8_1_1.get_sequence()[149:250]
+    assert sub_region.start == 5110483
+    assert sub_region.end == 5110636
