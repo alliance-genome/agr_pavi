2e7022b679ed070fc98397dcd44335b1536d6c79
Manuel Luypaert <luypaert@ebi.ac.uk>
2025-09-16 16:50:48 +0100

    Unit testing typing fix

--- webui/src/app/result/components/__tests__/InteractiveAlignment.test.tsx
+++ webui/src/app/result/components/__tests__/InteractiveAlignment.test.tsx
@@ -29,13 +29,38 @@ seq2        P-TL        3
 seq3        PKT-        3
 `;
 
+    const mockSeqInfoDict = {
+        'seq1': {
+            'embedded_variants': [
+                {
+                    'alignment_start_pos': 4,
+                    'alignment_end_pos': 4,
+                    'seq_start_pos': 4,
+                    'seq_end_pos': 4,
+                    'embedded_ref_seq_len': 1,
+                    'embedded_alt_seq_len': 1,
+                    'variant_id': 'mock:variant1',
+                    'seq_length': 1,
+                    'genomic_seq_id': 'chrX',
+                    'genomic_start_pos': 1,
+                    'genomic_end_pos': 1,
+                    'genomic_ref_seq': 'N',
+                    'genomic_alt_seq': 'N',
+                    'seq_substitution_type': 'substitution'
+                }
+            ]
+        },
+        'seq2': {},
+        'seq3': {},
+    }
+
     test('renders without crashing', () => {
-        const {container} = render(<InteractiveAlignment alignmentResult={mockAlignmentResult} />);
+        const {container} = render(<InteractiveAlignment alignmentResult={mockAlignmentResult} seqInfoDict={mockSeqInfoDict} />);
         expect(container.querySelector('#dd-colorscheme')).toBeInTheDocument();
     });
 
     test('displays the correct initial color scheme', () => {
-        const {container} = render(<InteractiveAlignment alignmentResult={mockAlignmentResult} />);
+        const {container} = render(<InteractiveAlignment alignmentResult={mockAlignmentResult} seqInfoDict={mockSeqInfoDict} />);
         const colorSchemeDefault = container.querySelector('#dd-colorscheme option[selected]')
         expect(colorSchemeDefault).toHaveValue('clustal2');
     });
